14ed7147b24503907f18f667f4b3502f78d4b14a
DanMarzo <marzogildan@gmail.com>
2022-09-22 22:34:11 -0300

    Create Footer

--- revisao/src/app/app.module.ts
+++ revisao/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
